cae3262ab0ce8dbbae083e37143bb9cf86abca86
slick-v <ronn.proff@gmail.com>
2026-07-13 23:49:24 +0530

    Backend:setup

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from config import settings
+
+
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello from the expense tracker backend", "env": settings.ENVIRONMENT}
