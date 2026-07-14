1b93d9d28aa6ea2526ee7ccf1fbad18f0ce10440
slick-v <ronn.proff@gmail.com>
2026-07-14 11:32:55 +0530

    Db-migrations

---
+++ backend/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    hashed_password = Column(String, nullable=False)
+    full_name = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
