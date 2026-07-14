d48a53ae952427bbdd4638624009d052c726c288
slick-v <ronn.proff@gmail.com>
2026-07-14 23:45:04 +0530

    React Setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+function App(){
+  return (
+    
+    <div className="min-h-screen flex items-center justify-center bg-slate-50">
+    <h1 className="text-3xl font-bold text-slate-800">Expense Tracker</h1>
+    </div>
+  )
+}
+
+
+export default App
